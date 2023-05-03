945a0856dac07e1e51b573864d0c9de144d6d063
Jebb Burditt <jebb.burditt@gmail.com>
2023-05-02 17:48:01 -0700

    working copy

---
+++ Sandbox/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Sandbox.Controllers
+{
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new FormViewModel();
+            model.Name = "Test123";
+            model.Code = 1;
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(FormViewModel formViewModel)
+        {
+            var test = formViewModel;
+
+            return View("Index", formViewModel);
+        }
+    }
+}
---
+++ Sandbox/ViewModels/FormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sandbox
+{
+    public class FormViewModel
+    {
+        [Required]
+        [MaxLength(7)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public int? Code { get; set; }
+
+        [Display(Name = "Address Line 1")]
+        public string AddressLine1 { get; set; }
+    }
+}
